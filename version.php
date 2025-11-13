16ad775620e8c204dfa388524121f628e79da3a0
Simon Gisler <simon.gisler@ffhs.ch>
2025-11-13 15:00:05 +0100

    Update README

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025061000;
+$plugin->version = 2025111300;
 $plugin->requires = 2024042203;
 $plugin->release = 'v4.4-r1';
 $plugin->component = 'block_course_checker_info';
